e710a8e8d5cad27590dce5c2ed65fe007b5ad781
Chanho <skylogin@gmail.com>
2016-07-02 23:53:59 +0900

    first commit

---
+++ source/components/Collection.react.js
@@ -0,0 +1,24 @@
+var React = require('react');
+
+var Collection = React.createClass({
+	render: function(){
+		return(
+			<div className="continer-fluid">
+				ddd
+			</div>
+		);
+	}
+});
+
+module.exports = Collection;
+
+/*
+class Collection extends React.component{
+	
+	render(){
+		return <div className="aa">aaa</div>;
+	}
+}
+
+export default Collection;
+*/
